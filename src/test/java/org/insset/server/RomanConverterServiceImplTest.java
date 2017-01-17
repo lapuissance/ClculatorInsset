a88ae1316bdf8325f4207f3f997aa41a7d3aab30
Alexandre Deloffre <alex62d@gmail.com>
2017-01-17 17:12:42 +0100

    Convert arabic to roman

---
+++ src/test/java/org/insset/server/RomanConverterServiceImplTest.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.insset.server;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author user
+ */
+public class RomanConverterServiceImplTest {
+    
+    public RomanConverterServiceImplTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of convertArabeToRoman method, of class RomanConverterServiceImpl.
+     */
+    @Test
+    public void testConvertArabeToRomanOK() {
+        System.out.println("convertArabeToRoman");
+        Integer nbr = 16;
+        RomanConverterServiceImpl instance = new RomanConverterServiceImpl();
+        String expResult = "XVI";
+        String result = instance.convertArabeToRoman(nbr);
+        assertEquals(expResult, result);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testConvertArabeToRomanLessThanOne() {
+        System.out.println("convertArabeToRomanInvalidInput");
+        Integer nbr = 0;
+        RomanConverterServiceImpl instance = new RomanConverterServiceImpl();
+        String result = instance.convertArabeToRoman(nbr);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testConvertArabeToRomanMoreThanTwoThousand() {
+        System.out.println("convertArabeToRomanInvalidInput");
+        Integer nbr = 2001;
+        RomanConverterServiceImpl instance = new RomanConverterServiceImpl();
+        String result = instance.convertArabeToRoman(nbr);
+    }
+}
